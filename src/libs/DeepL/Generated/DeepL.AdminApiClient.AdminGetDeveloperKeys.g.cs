adaf2bd8681675fef9940de749f0928c00b6b034
Konstantin S. <havendv@gmail.com>
2026-03-29 01:38:00 +0400

    feat: Updated OpenAPI spec (#3)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/DeepL/Generated/DeepL.AdminApiClient.AdminGetDeveloperKeys.g.cs
+++ src/libs/DeepL/Generated/DeepL.AdminApiClient.AdminGetDeveloperKeys.g.cs
@@ -233,7 +233,7 @@ namespace DeepL
                     __response.EnsureSuccessStatusCode();
 
                     return
-                        global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::DeepL.ApiKey>), JsonSerializerContext) as global::System.Collections.Generic.IList<global::DeepL.ApiKey> ??
+                        (global::System.Collections.Generic.IList<global::DeepL.ApiKey>?)global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::DeepL.ApiKey>), JsonSerializerContext) ??
                         throw new global::System.InvalidOperationException($"Response deserialization failed for \"{__content}\" ");
                 }
                 catch (global::System.Exception __ex)
@@ -264,7 +264,7 @@ namespace DeepL
                     ).ConfigureAwait(false);
 
                     return
-                        await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::DeepL.ApiKey>), JsonSerializerContext).ConfigureAwait(false) as global::System.Collections.Generic.IList<global::DeepL.ApiKey> ??
+                        (global::System.Collections.Generic.IList<global::DeepL.ApiKey>?)await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::DeepL.ApiKey>), JsonSerializerContext).ConfigureAwait(false) ??
                         throw new global::System.InvalidOperationException("Response deserialization failed.");
                 }
                 catch (global::System.Exception __ex)
