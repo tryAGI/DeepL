6462e03da3dececed5173945d45fef112da83828
Konstantin S. <havendv@gmail.com>
2026-08-25 05:41:29 +0400

    feat: Updated OpenAPI spec (#76)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/DeepL/Generated/DeepL.TranslateDocumentsClient.GetDocumentStatus.g.cs
+++ src/libs/DeepL/Generated/DeepL.TranslateDocumentsClient.GetDocumentStatus.g.cs
@@ -162,12 +162,13 @@ namespace DeepL
                     __httpRequest.Headers.Add(__authorization.Name, __authorization.Value);
                 } 
             }
-                            var __httpRequestContentBody = request.ToJson(JsonSerializerContext);
-                            var __httpRequestContent = new global::System.Net.Http.StringContent(
-                                content: __httpRequestContentBody,
-                                encoding: global::System.Text.Encoding.UTF8,
-                                mediaType: "application/x-www-form-urlencoded");
+                            var __formValues = new global::System.Collections.Generic.List<global::System.Collections.Generic.KeyValuePair<string, string>>();
+                            __formValues.Add(new global::System.Collections.Generic.KeyValuePair<string, string>(
+                                "document_key",
+                                request.DocumentKey1 ?? string.Empty));
+                            var __httpRequestContent = new global::System.Net.Http.FormUrlEncodedContent(__formValues);
                             __httpRequest.Content = __httpRequestContent;
+
                 global::DeepL.AutoSDKRequestOptionsSupport.ApplyHeaders(
                     request: __httpRequest,
                     clientHeaders: Options.Headers,
